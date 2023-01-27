ce2510b4e1b645d8ac77463875b25dd87c921335
unknown <campeloneto1@gmail.com>
2023-01-27 11:16:57 -0300

    input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputTextModule } from './input-text/input-text.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/input-text/input-text.component.ts
@@ -0,0 +1,75 @@
+import { Component, forwardRef, Input } from "@angular/core";
+import { AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors } from "@angular/forms";
+
+@Component({
+    selector: 'app-input-text',
+    templateUrl: './input-text.component.html',
+    styleUrls: ['./input-text.component.css'],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        multi: true,
+        useExisting: forwardRef(() => InputTextComponent)
+    },
+    {
+        provide: NG_VALIDATORS,
+        multi: true,
+        useExisting: InputTextComponent
+      },
+    ]   
+})
+
+export class InputTextComponent implements ControlValueAccessor{
+
+    inputvalor = '';
+    control!: AbstractControl;
+
+  
+  @Input() id!: string;
+  @Input() label!: string;
+  @Input() tipo!: string;
+  @Input() mascara!: string;
+
+  onChange = (inputvalor:string) => {};
+
+  onTouched = () => {};
+
+  touched = false;
+
+  disabled = true;
+
+  change() {
+    this.markAsTouched();
+    if (!this.disabled) {      
+      this.onChange(this.inputvalor);
+    }
+  }
+
+  writeValue(inputvalor: string) {
+    this.inputvalor = inputvalor;
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  setDisabledState(disabled: boolean) {
+    this.disabled = disabled;
+  }
+
+  validate(control: AbstractControl): ValidationErrors | void  {
+    this.control = control;
+    //console.log(control)
+  }
+
+}
